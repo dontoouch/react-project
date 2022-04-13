04d37e6e4e53743bfb89fd0c65faffb881b8391f
Alex <tereh.tnvd@gmail.com>
2022-04-14 01:07:00 +0300

    fix mistakes

--- react-pr/src/index.js
+++ react-pr/src/index.js
@@ -7,10 +7,10 @@ import Text from './Text';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Text text="item" textColor="red" textSize='50px' textDecoration={false}/>
-    <Text text="item" textColor="green" textSize='30px' textDecoration={true}/>
-    <Text text="item" textColor="yellow" textSize='20px' textDecoration={false}/>
-    <Text text="item" textColor="coral" textSize='40px' textDecoration={true} />
+    <Text text="Regular text" />
+    <Text text="Huge header" textSize='30px' />
+    <Text text="Danger notification" textColor="red"  />
+    <Text text="Underlined text" textSize='20px' textDecoration={true} />
   </React.StrictMode>
 );
 
