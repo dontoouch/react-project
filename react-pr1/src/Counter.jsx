c4a0ac6a170472977515963a7bb5931797473fd1
Alex <tereh.tnvd@gmail.com>
2022-04-18 20:52:16 +0300

    task 6

---
+++ react-pr1/src/Counter.jsx
@@ -0,0 +1,30 @@
+
+import './Counter.css';
+
+import {useState} from 'react'
+
+function Counter() {
+const [ checkbox , setCheckbox] = useState('active');
+
+
+
+
+
+  return (
+    <div className="checkbox">
+      <button onClick={()=> {
+      if(checkbox === "active"){
+        setCheckbox('deactiv')
+      }
+      else {
+        setCheckbox('active')
+      }
+      
+    }}>
+        {checkbox}
+      </button>
+    </div>
+  );
+}
+
+export default Counter;
